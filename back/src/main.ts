606c3168874c5107e3e1e9d743d53dcf5570de6b
alexantos <xandreale09@gmail.com>
2025-11-13 23:26:33 -0300

    Adiciona open api.

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,16 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const corsOptions: CorsOptions = {
+	const app = await NestFactory.create(AppModule);
+	const corsOptions: CorsOptions = {
 		origin: ['http://localhost:3000'],
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 		credentials: true,
 		allowedHeaders: 'Content-Type, Accept, Authorization',
 	};
 	app.enableCors();
-  await app.listen(process.env.PORT ?? 3001);
+	const config = new DocumentBuilder()
+		.setTitle('Back')
+		.setDescription('Uma aplicação AG sistemas')
+		.setVersion('1.0')
+		.addTag('back')
+		.build();
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, documentFactory);
+
+	await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
